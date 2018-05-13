d68fec9c1a4dfa0402a8067c7547daa922bb2012
jaimeabbariao <jaimeabbariao@gmail.com>
2018-05-13 12:14:09 -0400

    initial commit

---
+++ ratings-counter.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf=conf)
+
+lines = sc.textFile("./ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.iteritems():
+    print("{}, {}".format(key, value))
+
+# to run this file, write spark-submit filename.py
